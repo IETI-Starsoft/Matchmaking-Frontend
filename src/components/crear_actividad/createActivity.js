f0489e45d4514034602a8a6e307548e9c4ac9ebb
diego2097 <ye.yo14@hotmail.com>
2020-02-20 12:27:47 -0500

    :iphone: create-activity

--- src/components/crear_actividad/createActivity.js
+++ src/components/crear_actividad/createActivity.js
@@ -271,7 +271,7 @@ const styles = theme => ({
   paper: {
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(3),
-    padding: 0,
+    padding: theme.spacing(1),
     [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
       marginTop: theme.spacing(6),
       marginBottom: theme.spacing(6),
