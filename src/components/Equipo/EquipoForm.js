34508db6d6f380ec467d1b622010effa4884665e
luis572 <luispg572@gmail.com>
2020-03-26 12:28:54 -0500

    :sparkles: Conexion entre el api y la creacion de equipos

--- src/components/Equipo/EquipoForm.js
+++ src/components/Equipo/EquipoForm.js
@@ -3,24 +3,32 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 
-export default function AddressForm() {
+export default function AddressForm({props}) {
+  const handleChange = e => {
+    e.preventDefault();
+    props(e.target.value);
+    
+  };
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        General
+      <from>
+        <Typography variant="h6" gutterBottom>
+          General
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="Ename"
-            name="Ename"
-            label="Nombre Equipo"
-            fullWidth
-            autoComplete="billing address-line1"
-          />
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="Ename"
+              name="Ename"
+              label="Nombre Equipo"
+              onChange={handleChange}
+              fullWidth
+              autoComplete="billing address-line1"
+            />
+          </Grid>
         </Grid>
-      </Grid>
+        </from>
     </React.Fragment>
   );
 }
