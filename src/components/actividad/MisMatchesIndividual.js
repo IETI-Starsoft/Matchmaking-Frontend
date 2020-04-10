7cd55e2841122d98887737bc0902a9bb61990940
diego2097 <ye.yo14@hotmail.com>
2020-04-09 20:10:09 -0500

    :construction: Getting list of activities working for both views

---
+++ src/components/actividad/MisMatchesIndividual.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react';
+import { ActividadCard } from './ActividadCard';
+import Grid from '@material-ui/core/Grid';
+import Menu from "../menu/NavBar";
+import Filtros from './Filtros'; 
+import ModalMasInfo from './ModalMasInfo'
+import axiosHeader from '../../api/axiosHeader';
+
+export class MisMatchesIndividual extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {activities: []};
+        this.getAllActivities = this.getAllActivities.bind(this);
+        this.getAllActivities();
+    }
+
+    getAllActivities(){
+        var temp = []
+        let user = JSON.parse(localStorage.getItem("user"));
+        axiosHeader.get("users/id/" + user.userId + "/activities")
+          .then(response => {
+            response.data.map(value => {
+              temp.push(value);
+            })
+            this.setState({activities:temp})
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    render() {
+        return (
+        
+           <Fragment>
+                <Menu />
+                <Filtros />  
+                <Grid container spacing={32} justify="center">
+                    {this.state.activities.map((actividad, i) => {
+                        return (
+                            <ActividadCard props={actividad} />
+                        );
+                    })}
+                </Grid>
+            </Fragment>
+        ); 
+
+
+    }
+
+}
