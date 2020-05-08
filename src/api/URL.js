cfb807249b3784fd4e15abe6aa154fa2bdef9c4d
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2020-05-08 01:00:59 -0500

    :bug: Fix max stack when image is too large

---
+++ src/api/URL.js
@@ -0,0 +1,4 @@
+
+const BACKEND_URL = "https://matchmaking-iback.herokuapp.com";
+
+export default BACKEND_URL;
--- src/api/axiosHeader.js
+++ src/api/axiosHeader.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import BACKEND_URL from "./URL";
 
 const axiosHeader = axios.create({
-  baseURL: "https://matchmaking-iback.herokuapp.com/api",
-  timeout: 8000,
-  headers: { Authorization: "Bearer " + localStorage.getItem("accessToken") }
+    baseURL: `${BACKEND_URL}/api`,
+    timeout: 8000,
+    headers: { Authorization: "Bearer " + localStorage.getItem("accessToken") }
 });
 
-export default axiosHeader;
+export default axiosHeader ;
