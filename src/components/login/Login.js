8cbc5c9aff031e9d2b1f9ebb9a4fc7a2865c74f7
luis572 <luispg572@gmail.com>
2020-05-21 13:07:52 -0500

    :lipstick: Cambio de estilos en el login y register
    Co-authored-by: cdavidd <cdavidlopez@hotmail.com>

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,25 +1,21 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import logo from "./logoM.webp";
+import logo from "./logoM.jpeg";
 import axios from "axios";
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
+    <Typography variant="body2" style={{color:"#71db77"}} color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" style={{color:"#71db77"}} href="https://material-ui.com/">
         Your Website
       </Link>{" "}
       {new Date().getFullYear()}
@@ -28,7 +24,37 @@ function Copyright() {
   );
 }
 
+
+
 const useStyles = makeStyles(theme => ({
+  login: {
+    background: "linear-gradient(45deg, #71db77 30%, #23f57e 90%)",
+    border: 0,
+    borderRadius: 15,
+    //boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "#242424",
+    width: "%",
+    height: "70%",
+    padding: "0 30px",
+
+  },
+  cssLabel: {
+    color: 'white'
+  },
+
+  cssOutlinedInput: {
+    '&$cssFocused $notchedOutline': {
+      borderColor: `${theme.palette.primary.main} !important`,
+      color:"white"
+    }
+  },
+
+  cssFocused: {},
+
+  notchedOutline: {
+    borderWidth: '1px',
+    borderColor: 'white !important'
+  },
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
@@ -39,6 +65,8 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.secondary.main
   },
   logo: {
+    width: "35%",
+    height: "35%",
     margin: theme.spacing(1)
   },
   form: {
@@ -46,7 +74,17 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "#71db77",
+    borderRadius: 15,
+  },
+  main: {
+    position: "absolute",
+    top: "0%",
+    left: "0%",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#222222",
   }
 }));
 
@@ -55,82 +93,110 @@ export default function SignIn() {
   const handleSubmit = e => {
     e.preventDefault();
     axios.post('https://matchmaking-iback.herokuapp.com/users/login', {
-             email: e.target.email.value,
-             password: e.target.password.value
+      email: e.target.email.value,
+      password: e.target.password.value
     })
-    .then(function(response){
-      localStorage.setItem("accessToken",response.data.accessToken);
-      localStorage.setItem("user", JSON.stringify(response.data.user));
-      window.location.href = "/perfil";
-    })
-    .catch(function(error){
-      alert("Check credentials");
-    });
-    
+      .then(function (response) {
+        localStorage.setItem("accessToken", response.data.accessToken);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        window.location.href = "/perfil";
+      })
+      .catch(function (error) {
+        alert("Check credentials");
+      });
+
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <img src={logo} alt="logo" className={classes.logo} />
+    <div className={classes.main}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <img src={logo} alt="logo" className={classes.logo} />
 
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign In
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              InputLabelProps={{
+                style: {color: 'white'},
+                classes: {
+                  
+                  root: classes.cssLabel,
+                  focused: classes.cssFocused,
+                  color:classes.text
+                },
+              }}
+              InputProps={{
+                style: {color: 'white'},
+                classes: {
+                  root: classes.cssOutlinedInput,
+                  focused: classes.cssFocused,
+                  notchedOutline: classes.notchedOutline,
+                },
+              }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              InputLabelProps={{
+                style: {color: 'white'},
+                classes: {
+                  root: classes.cssLabel,
+                  focused: classes.cssFocused,
+                },
+              }}
+              InputProps={{
+                style: {color: 'white'},
+                classes: {
+                  root: classes.cssOutlinedInput,
+                  focused: classes.cssFocused,
+                  notchedOutline: classes.notchedOutline,
+                },
+              }}
+              id="password"
+              autoComplete="current-password"
+            />
+            <br></br>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+
+              className={classes.submit}
+            >
+              Sign In
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" style={{color:"#71db77"}} variant="body2">
+                  Forgot password?
               </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/sign-up" style={{color:"#71db77"}} variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="/sign-up" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    </div>
   );
 }
